4c3a901bbae5e6411cd37cb15045ec48583c8979
kchyzhyk <kchyzhykova888@gmail.com>
2024-11-25 11:34:03 +0100

    upd

--- backend/generate_static.py
+++ backend/generate_static.py
@@ -13,7 +13,7 @@ output_dir = os.path.join(settings.BASE_DIR, 'backend/docs')
 os.makedirs(output_dir, exist_ok=True)
 
 rendered_content = render_to_string('tasks/base.html',
-    {'csrf_token': ''}
+    # {'csrf_token': ''}
 )
 
 with open(os.path.join(output_dir, 'index.html'), 'w', encoding='utf-8') as f:
